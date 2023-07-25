3fb7a7e21ced03854b2b538876b466d8a88da856
Simon Duranowski <sduranowski1@gmail.com>
2023-07-26 00:05:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 //Constructor for favicon
 const favicon = document.querySelector("link[rel='icon']");
 
-//Fixed favicon
+
+//Fixed favicon (in my case, it is a pink hollow heart)
 favicon.setAttribute("href", "https://freepngimg.com/thumb/heart/37323-9-pink-heart.png")
 
-//var flag = true;
 
 //Timer function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//Infinite loop for blinking favicon
+
+//Infinite loop for blinking favicon (in this example, it will turn the pink hollow heart into the red full one)
 var demo = async function () {
     while (true) {
         favicon.setAttribute("href", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png")
         await sleep(1000)    
-        favicon.setAttribute("href",  "https://freepngimg.com/thumb/heart/37323-9-pink-heart.png")
+        favicon.setAttribute("href", "https://freepngimg.com/thumb/heart/37323-9-pink-heart.png")
         await sleep(1000)
     } 
 }
 
-//Non-successful JS blinking favicon code   
+
+//Non-successful JS blinking favicon code (more complicated, unfinished code) 
 const target = document.querySelector(".counter")
 
 const callback = (entries) => {
@@ -48,15 +50,15 @@ var Observer = async function() {
     }
 }
 
+//Observer()  
 
 
 //Successful JS(jQuery) blinking favicon code   
 var interval = setInterval(function () {
+    //Replace '.counter' className with any web element that will trigger your favicon function once the element is visible
     if ($('.counter').is(':visible')) {
         demo()
     }
 })
 
 interval
-
-//Observer()   
